8079a56f5f99d92a49c26353aa94ba1d92b2fbf0
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2025-04-14 22:16:15 -0600

    Clean up figs

--- symmetry1/figs/doasy.sh
+++ symmetry1/figs/doasy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 asy -f pdf trinimfig
-latexmk -pdf trinim.tex
 
 asy -f pdf -u"N=1" trinimfig -o trinimfig1
 asy -f pdf -u"N=2" trinimfig -o trinimfig2 
 asy -f pdf -u"N=3" trinimfig -o trinimfig3 
 asy -f pdf -u"N=4" trinimfig -o trinimfig4 
 asy -f pdf -u"N=5" trinimfig -o trinimfig5 
-latexmk -pdf trinims.tex
